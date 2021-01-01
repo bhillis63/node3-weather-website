74c723db578412c09453da0a30715ed7687efadf
Brian Hillis <bhillis@avaya.com>
2021-01-01 11:46:32 -0700

    Added more to user interface. Wired up querySelectors

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,45 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+// fetch('http://localhost:3000/weather?address=boston').then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             console.log(data.error);
+//         } else {
+//             console.log(data.location);
+//             console.log(data.forcast);
+//         }
+            
+//     })
+// })
+const weatherform = document.querySelector('form');
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+
+
+weatherform.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const location = search.value;
+    //console.log(location);
+
+    messageTwo.textContent = 'Loading....'
+    messageOne.textContent = '';
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
-        console.log(data);
+        messageTwo.textContent = '';
+        if (data.error) {
+            console.log(data.error);
+            messageOne.textContent = data.error;
+        } else {
+
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forcast;
+            // console.log(data.location);
+            // console.log(data.forcast);
+        }
+            
     })
 })
-
-fetch('')
+})
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     //const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
-    const url = 'http://api.weatherstack.com/forecast?6c42e93165d7233f6a885e1bf7b37b08&query=' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=6c42e93165d7233f6a885e1bf7b37b08&query=' + latitude + ',' + longitude
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + '. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
